292a3069cee3a128cf8dee90efe1102ac8fc9817
Mia Zhu <18340826605@163.com>
2022-08-03 00:18:39 +0800

    feat：回文子串 过滤条件踩坑了 记录一下

--- algorithms/DP/647. Palindromic_Substrings/PalindromicSubstrings.h
+++ algorithms/DP/647. Palindromic_Substrings/PalindromicSubstrings.h
@@ -22,9 +22,9 @@ public:
     int res = 0;
     for (int i = s.size() - 1; i >= 0; i--) {
       for (int j = i; j < s.size(); j++) {
-        if (s[i] != s[j])
+        if (s[i] != s[j]) // "ab" 这里注意过滤规则是判断俩字符是否相等
           continue;
-        if (j - i <= 1) {// "a"|"aa"
+        if (j - i <= 1) { // "a"|"aa"
           res++;
           dp[i][j] = true;
         } else if (dp[i + 1][j - 1]) {//"cabac" i为第一个a的索引 j为倒数第一个a的索引
